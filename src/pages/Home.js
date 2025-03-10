d03f75387b013fbff52a6f8f60fa8fb1027833e3
SammySho <sammy.shorthouse@hotmail.com>
2025-03-10 23:11:47 +0000

    feat: added financial modelling as submodule fix: corrected some CV info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,9 +11,9 @@ const Home = () => {
           alt="Profile"
           className="profile-image"
         />
-        <h1>Welcome to my website</h1>
-        <p>Explore my education, experience, interests, and recent projects.</p>
-        
+        <h1>Hi! I'm Sammy</h1>
+        <p>I'm a PhD student in Engineering Mathematics at the University of Bristol, working on developing computer vision models for the predictive modelling of brain organoid development.</p>
+        <p>Have a look at my education, experience, interests, and some of my recent projects using the links above, feel free to reach out if you'd like to chat!</p>
         <div className="social-links">
           <a href="https://github.com/SammySho" target="_blank" rel="noopener noreferrer">
             <FaGithub />
